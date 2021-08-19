9921b301360f25127144bebded9184c3112d7039
Ronin ssw <roninssw@RonindeMacBook-Pro.local>
2021-08-19 08:26:41 +0800

    array demo practise

---
+++ go_learning_imook/demo_array.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main(){
+	//定义数组
+	var arr1 [5]int
+	arr2 := [4]int{1, 2, 4, 5}
+	arr3 := [...]int{1, 2, 4, 5, 6, 8}
+	var grid [4][5]int // 二维数组4行5列
+	fmt.Println(arr1, arr2, arr3)
+	fmt.Println(grid)
+	//遍历数组方式一
+	//for i := 0; i < len(arr3); i++{
+	//	fmt.Println(arr3[i])
+	//}
+
+	//遍历数组方式二 range
+	//for i := range arr3{
+	//	fmt.Println(arr3[i])
+	//}
+
+	//遍历数组并获取下标和值
+	//for i, v := range arr3{
+	//	fmt.Println(i, v)
+	//}
+
+	//遍历数组只获取值
+	for _, v := range arr3{
+		fmt.Println(v)
+	}
+}
