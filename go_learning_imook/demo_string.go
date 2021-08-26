2c7eafec9d7b0c94fc646abccc866efd0a944c99
roninsswang <roninsswang@roninsswangdeMacBook-Pro.local>
2021-08-26 23:26:19 +0800

    demo rune practise

---
+++ go_learning_imook/demo_string.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main(){
+	s := "Yes我爱goLang!"
+	fmt.Println(s)
+
+	for _, b := range []byte(s) {
+		fmt.Printf("%X ", b)
+	}
+	fmt.Println()
+	for i, ch := range s{  // ch is a rune
+		fmt.Printf("(%d %X)", i, ch)
+	}
+
+	fmt.Println("Rune count: ",
+		utf8.RuneCountInString(s))
+
+	bytes := [] byte(s)
+	for len(bytes) > 0 {
+		ch, size := utf8.DecodeRune(bytes)
+		bytes = bytes[size:]
+		fmt.Printf("%c ", ch)
+	}
+	fmt.Println()
+
+	for i , ch := range []rune(s){
+		fmt.Printf("(%d %c)", i, ch)
+	}
+	fmt.Println()
+
+// rune 相当于go的char
+
+}
